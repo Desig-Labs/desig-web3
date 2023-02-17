21b12882ee8b8676b2ee8a501e85c0b4ba022d2b
tuphan-dn <tuphan@descartes.network>
2023-02-17 12:06:29 +0700

    send real tx on sol

--- test/config.ts
+++ test/config.ts
@@ -1,6 +1,74 @@
-// export const cluster = 'https://eddsa.desig.dev'
-export const cluster = 'http://localhost:10000'
+import {
+  Cluster,
+  Connection,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from '@solana/web3.js'
+
+export const cluster = 'https://eddsa.desig.dev'
 export const aliceSecret =
   'eddsa/4JKKpEK3teGFFbxeTFzScBaYse55mH4LgbtYG3q4BcKv/2AFv15MNPuABCdZq1uf8PCPa2gBYrgjpLxDu1mGCiBkfdqvP3ahydUXYF1YshvsCARgpvgt5wHcghGUfQxghGEf'
 export const bobSecret =
   'eddsa/4JKKpEK3teGFFbxeTFzScBaYse55mH4LgbtYG3q4BcKv/3KWq19hjnoKJG4LJ71d4R6kZJQ1AMe7eL7tZpLsUD7Uq2rmEra5eGGeCRQWgDvpYtneHzY8qBS58hPjVSpJAviy'
+
+const connection = new Connection('https://devnet.genesysgo.net', 'confirmed')
+
+export const transfer = async (payer: PublicKey) => {
+  const tx = new Transaction()
+  const ix = SystemProgram.transfer({
+    fromPubkey: payer,
+    toPubkey: new PublicKey('8W6QginLcAydYyMYjxuyKQN56NzeakDE3aRFrAmocS6D'),
+    lamports: 5000,
+  })
+  tx.add(ix)
+  tx.feePayer = payer
+  tx.recentBlockhash = (
+    await connection.getLatestBlockhash('confirmed')
+  ).blockhash
+  return tx
+}
+
+export const sendAndConfirm = async (tx: Transaction) => {
+  const signature = await connection.sendRawTransaction(tx.serialize(), {
+    skipPreflight: true,
+    preflightCommitment: 'confirmed',
+  })
+  const { blockhash, lastValidBlockHeight } =
+    await connection.getLatestBlockhash()
+  await connection.confirmTransaction({
+    signature,
+    blockhash,
+    lastValidBlockHeight,
+  })
+  return signature
+}
+
+export const print = (...args: any[]) => {
+  console.group()
+  console.log('\x1b[36m↳\x1b[0m', ...args, '')
+  console.groupEnd()
+}
+
+export const isSolanaAddress = (
+  address: string | undefined,
+): address is string => {
+  if (!address) return false
+  try {
+    const publicKey = new PublicKey(address)
+    if (!publicKey) throw new Error('Invalid public key')
+    return true
+  } catch (er) {
+    return false
+  }
+}
+
+export const solscan = (
+  addressOrTxId: string,
+  net: Cluster = 'devnet',
+): string => {
+  if (isSolanaAddress(addressOrTxId)) {
+    return `https://solscan.io/account/${addressOrTxId}?cluster=${net}`
+  }
+  return `https://solscan.io/tx/${addressOrTxId}?cluster=${net}`
+}
--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -1,15 +1,31 @@
-import { utils } from '@noble/ed25519'
-import { encode } from 'bs58'
+import { PublicKey, Transaction as SolTransaction } from '@solana/web3.js'
+import { decode, encode } from 'bs58'
 import { expect } from 'chai'
 import { DesigEdDSAKeypair, Transaction } from '../dist'
-import { cluster, aliceSecret, bobSecret } from './config'
+import {
+  cluster,
+  aliceSecret,
+  bobSecret,
+  transfer,
+  sendAndConfirm,
+  print,
+  solscan,
+} from './config'
 
 describe('transaction', () => {
-  const alice = new Transaction(cluster, new DesigEdDSAKeypair(aliceSecret))
-  const bob = new Transaction(cluster, new DesigEdDSAKeypair(bobSecret))
-  const message = utils.randomBytes(32)
+  const aliceKeypair = new DesigEdDSAKeypair(aliceSecret)
+  const bobKeypair = new DesigEdDSAKeypair(bobSecret)
+  const alice = new Transaction(cluster, aliceKeypair)
+  const bob = new Transaction(cluster, bobKeypair)
+  const masterkey = new PublicKey(aliceKeypair.masterkey)
+  let tx: SolTransaction
+
+  before(async () => {
+    tx = await transfer(masterkey)
+  })
 
   it('initialize transaction', async () => {
+    const message = tx.serializeMessage()
     const txId = Transaction.deriveTxId(message)
     const { msg, id } = await alice.initialize({ message })
     expect(msg).equal(encode(message))
@@ -17,6 +33,7 @@ describe('transaction', () => {
   })
 
   it('fetch transaction', async () => {
+    const message = tx.serializeMessage()
     const txId = Transaction.deriveTxId(message)
     const { id, msg } = await alice.fetch(txId)
     expect(msg).equal(encode(message))
@@ -24,6 +41,7 @@ describe('transaction', () => {
   })
 
   it('approve transaction by alice', async () => {
+    const message = tx.serializeMessage()
     const txId = Transaction.deriveTxId(message)
     const { msg, id } = await alice.approve(txId)
     expect(msg).equal(encode(message))
@@ -31,17 +49,23 @@ describe('transaction', () => {
   })
 
   it('approve transaction by bob', async () => {
+    const message = tx.serializeMessage()
     const txId = Transaction.deriveTxId(message)
     const { msg, id } = await bob.approve(txId)
     expect(msg).equal(encode(message))
     expect(txId).equal(id)
   })
 
-  it('finalize/verify transaction', async () => {
+  it('finalize/verify/submit transaction', async () => {
+    const message = tx.serializeMessage()
     const txId = Transaction.deriveTxId(message)
     const sig = await bob.finalize(txId)
     const ok = await bob.verify(txId, sig)
+    tx.addSignature(masterkey, Buffer.from(decode(sig)))
+    const txHash = await sendAndConfirm(tx)
     expect(sig).not.empty
     expect(ok).to.be.true
+    expect(txHash).not.empty
+    print(solscan(txHash))
   })
 })
