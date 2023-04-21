0748cc045cd692281f85149a9f1360f7434ce775
tuphan-dn <tuphan@descartes.network>
2023-04-21 16:05:59 +0700

    support signSync

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { utils } from '@noble/ed25519'
+import * as ec from '@noble/secp256k1'
+import * as ed from '@noble/ed25519'
 import { sha512 } from '@noble/hashes/sha512'
+import { sha256 } from '@noble/hashes/sha256'
+import { hmac } from '@noble/hashes/hmac'
+import { concatBytes } from '@noble/hashes/utils'
 // @noble/ed25519 patch
-utils.sha512Sync = (...m) => sha512(utils.concatBytes(...m))
+ed.utils.sha512Sync = (...m) => sha512(concatBytes(...m))
+// @noble/secp256k1 patch
+ec.utils.hmacSha256Sync = (k, ...m) => hmac(sha256, k, concatBytes(...m))
 
 export * from './types'
 export * from './keypair'
